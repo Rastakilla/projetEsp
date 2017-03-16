6b82e8cd9a1b3e698b311065a76d87908e5f12ef
Rastakilla <cednoel@live.ca>
2017-03-16 11:42:00 -0400

    ajout changement pour shits

---
+++ includes/Footer.php
@@ -0,0 +1,7 @@
+ <nav id="footer">
+        <div class="container">
+            <div class="pull-left fnav">
+                <p>TOUS DROITS RÉSERVÉS. <a class = 'color'> CÉDÉRIC NOËL</a> COPYRIGHT © 2017</p>
+            </div>
+        </div>
+    </nav>
